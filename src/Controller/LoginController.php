d4507b806f25075c40bdebdba6c9971ed8491b99
Arbi <developer.mediseo@gmail.com>
2022-12-19 18:38:41 +0100

    login

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+// src/Controller/LoginController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name:'app_login')]
+function index(AuthenticationUtils $authenticationUtils): Response
+    {
+    // get the login error if there is one
+    $error = $authenticationUtils->getLastAuthenticationError();
+
+    // last username entered by the user
+    $lastUsername = $authenticationUtils->getLastUsername();
+
+    return $this->render('pages/login/login.html.twig', [
+        'controller_name' => 'LoginController',
+        'last_username' => $lastUsername,
+        'error' => $error,
+    ]);
+
+
+    
+
+}
+}
